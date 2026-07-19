93ce4e843529426df902e4069f6040495b48dea4
mohammad <mohammadbabakhani2003@gmail.com>
2026-07-19 17:28:09 +0330

    change percent color

--- src/components/pages/Skill.jsx
+++ src/components/pages/Skill.jsx
@@ -29,14 +29,25 @@ const Skill = ({ name, icon, color, darkColor, colorB, value }) => {
           sx={{ p: 3, backgroundColor: colorB, fontWeight: 600 }}
         />
       </Divider>
-      <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", mb: 5 }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          mb: 5,
+        }}
+      >
         <Box sx={{ minWidth: 35, mr: 1 }}>
           <Typography variant="body2" color="#000">
             <Badge
               variant="standard"
               badgeContent={`${value}%`}
               //   color={color}
-              sx={{ backgroundColor: theme.palette.mode === 'dark' ? darkColor :  color }}
+              sx={{
+                backgroundColor:
+                  theme.palette.mode === "dark" ? darkColor : color,
+                color: theme.palette.mode === "dark" ? "white" : "black",
+              }}
             />
           </Typography>
         </Box>
@@ -48,11 +59,14 @@ const Skill = ({ name, icon, color, darkColor, colorB, value }) => {
             sx={{
               height: 10,
               borderRadius: 2,
-              backgroundColor: `${theme.palette.mode === 'dark' ? darkColor : color}33`, // 0.2 opacity (hex)
+              backgroundColor: `${
+                theme.palette.mode === "dark" ? darkColor : color
+              }33`, // 0.2 opacity (hex)
 
               "& .MuiLinearProgress-bar": {
-                backgroundColor: theme.palette.mode === 'dark' ? darkColor : color,
-                // backgroundColor: theme.palette.mode === 'dark' ? '#032530' : "#F8FAFC", 
+                backgroundColor:
+                  theme.palette.mode === "dark" ? darkColor : color,
+                // backgroundColor: theme.palette.mode === 'dark' ? '#032530' : "#F8FAFC",
                 borderRadius: 2,
               },
             }}
